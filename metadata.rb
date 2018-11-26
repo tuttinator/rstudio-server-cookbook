8a0b878ebebae5d75d828f4ae83d77a9852c9b72
Caleb Tutty <caleb.tutty@gmail.com>
2018-11-26 19:37:45 +1300

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'caleb.tutty@gmail.com'
 license 'All Rights Reserved'
 description 'Installs RStudio Server'
 long_description 'Installs RStudio Server'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 13.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
